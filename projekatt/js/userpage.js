a59920f10059c64bd77af0370f8f94479bc43971
bajoPivljanin <103775541+bajoPivljanin@users.noreply.github.com>
2023-03-01 22:47:19 +0100

    eo

--- projekatt/js/userpage.js
+++ projekatt/js/userpage.js
@@ -4,14 +4,12 @@ var dataParam = urlParams.get('data');
 
 var myData = JSON.parse(decodeURIComponent(dataParam));
 
-
 window.addEventListener('load',()=>{
     
     document.querySelector('#searchUsername').innerText = myData.Username;
     document.querySelector('#searchEmail').innerText = myData.email;
     getUsersPosts();
 })
-
 
 //Search bar nestaje
 const searchBar = document.querySelector(".navsearch");
@@ -26,27 +24,29 @@ document.addEventListener("click", function(event) {
   }
 });
 
-
 async function getUsersPosts(){
   let all_posts = new Post()
   all_posts = await all_posts.getUserPost(myData.userId)
-  console.log(all_posts)
+  
   all_posts.forEach(post => {
       async function getPostUser() {
          
           let user = new User()
           user = await user.get(post.user_id)
 
+          
           let comments = new Comment()
           comments = await comments.get(post.id)
+          
+          /*let usercom = new User()
+          usercom = await usercom.get(comments.username)*/
+          
           let comments_html = ''
           if(comments.length > 0){
               comments.forEach(comment => {
-                  comments_html += `<div class="single-comment">${comment.content} autor ${comments.user_id}</div>`
+                  comments_html += `<div class="single-comment">${comment.content} autor ${'treba user da pise'} </div>`
               })
           } 
-         
-
           let html =  document.querySelector('#allPostsWrapper').innerHTML
           let delete_post_html = ''
 
