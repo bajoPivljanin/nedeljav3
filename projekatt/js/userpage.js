aca425bb5a5ed60cce1576db2c0889298c252de1
UltraHardCorn25 <108307199+UltraHardCorn25@users.noreply.github.com>
2023-03-01 13:01:59 +0100

    Search bar nestaje kad kliknes na neki deo sajta

--- projekatt/js/userpage.js
+++ projekatt/js/userpage.js
@@ -1,12 +1,24 @@
 
 window.addEventListener('load',()=>{
-    console.log('aaaaa')
     var queryString = window.location.search;
     var urlParams = new URLSearchParams(queryString);
     var dataParam = urlParams.get('data');
 
     var myData = JSON.parse(decodeURIComponent(dataParam));
-    console.log(myData.Username);
     document.querySelector('#searchUsername').innerText = myData.Username;
     document.querySelector('#searchEmail').innerText = myData.email;
 })
+
+
+//Search bar nestaje
+const searchBar = document.querySelector(".navsearch");
+let list = document.getElementById("userneki");
+// Add a click event listener to the document object
+document.addEventListener("click", function(event) {
+  // Check if the target of the click event is inside the search bar
+  if (!searchBar.contains(event.target) && !list.contains(event.target)) {
+    // If the target is not inside the search bar, hide the search bar
+    list.innerHTML ="";
+    list.style.opacity="0";
+  }
+});
