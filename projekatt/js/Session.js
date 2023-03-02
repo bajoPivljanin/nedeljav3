64952ab92f8c3529dd6dbd21bc17ebe029e7243c
UltraHardCorn25 <108307199+UltraHardCorn25@users.noreply.github.com>
2023-03-02 12:47:07 +0100

    loadscreen

--- projekatt/js/Session.js
+++ projekatt/js/Session.js
@@ -35,3 +35,12 @@ class Session {
         }
     }
 }
+window.addEventListener('load',() => {
+    
+    //document.querySelector('.loadScreen').style.display="none";
+    setTimeout(() => { 
+        document.querySelector('.loadScreen').style.opacity="0"
+        document.querySelector('.loadScreen').style.zIndex="-100"}, 2000);
+    
+    
+})
